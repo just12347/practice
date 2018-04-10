2a18cb97f91b943f3cb1151e4443934a84c4f7b9
mikeyandleo <b01705031@ntu.edu.tw>
2018-04-10 16:35:12 +0800

    FactorialTrailingZeros

---
+++ 172. FactorialTrailingZeros.py
@@ -0,0 +1,14 @@
+class Solution:
+    def trailingZeroes(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        count = 0
+        if n == 0:
+            return 0
+        else:
+            while n >0:
+                n = n//5
+                count += n
+        return count
